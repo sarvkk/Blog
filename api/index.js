719b9f1a7b7b315e453fb84d443625d050090f8f
sarvkk <sarvajitkhadka444@gmail.com>
2024-09-24 17:28:47 +0545

    testapi

--- api/index.js
+++ api/index.js
@@ -1,13 +1,22 @@
 import express from 'express'
 const app= express()
+
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
-
+import userRoutes from './routes/user.route.js'    
+import authRoutes from './routes/auth.route.js'
 dotenv.config()
+
+app.use(express.json())
 
 mongoose
 .connect(process.env.MONGO)
-    .then(()=>{console.log('Database connected')},
-    (err)=>{console.log(err)}
-)
+    .then(()=>{
+        console.log('Database connected')
+    })
+    .catch((err)=>{
+    console.log(err)})
 app.listen('3000',()=>{ console.log('Server is running on port 3000')}) 
+
+app.use('/api/user',userRoutes)
+app.use('/api/auth',authRoutes)
