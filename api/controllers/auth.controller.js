8bc1171c2d96d5f2e452e591de08831835c0014c
sarvkk <sarvajitkhadka444@gmail.com>
2024-09-25 15:07:13 +0545

    Add function to signup page

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,12 +1,21 @@
 import User from "../models/user.model.js"
+import bcryptjs from 'bcryptjs'
+import errorHandler from '../utils/error.js'
 
-export const signup =  async (req,res)=>{
+export const signup =  async (req,res,next)=>{
     const {username,email,password}= req.body;
-    if(!username || !email || !password || username===''|| email===''|| password===''){
-        return res.status(400).json({error:"All fields are required"})
+    if(!username || 
+        !email || 
+        !password || 
+        username===''
+        || email===''
+        || password===''
+    ) {
+        next(errorHandler(400,'All fields are required'))
     }
+    const hashedPassword=bcryptjs.hashSync(password,10); 
     const newUser=new User({
-        username,email,password
+        username,email,password:hashedPassword
     });
 
     try{
@@ -16,9 +25,8 @@ export const signup =  async (req,res)=>{
 
     }
     catch(err){
-        return res.status(500).json({error:"Something went wrong"})
+        next(err);
     }
-     
 }
 
 
--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import userRoutes from './routes/user.route.js'    
 import authRoutes from './routes/auth.route.js'
+import e from 'express'
 dotenv.config()
 
 app.use(express.json())
@@ -21,3 +22,13 @@ app.listen('3000',()=>{ console.log('Server is running on port 3000')})
 
 app.use('/api/user',userRoutes)
 app.use('/api/auth',authRoutes)
+
+app.use((err,req,res,next)=>{
+    const statusCode=err.statusCode || 500;
+    const message=err.message || 'Internal Server Error'
+    res.status(statusCode).json({
+        success:false,
+        statusCode,
+        message
+    })
+})
